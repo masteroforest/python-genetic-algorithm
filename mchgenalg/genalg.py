62fe291537600ffc6a62df148d2810a847904bb3
masteroforest <31575787+masteroforest@users.noreply.github.com>
2019-11-19 14:48:28 +0300

    first com

--- mchgenalg/genalg.py
+++ mchgenalg/genalg.py
@@ -7,8 +7,11 @@ Based on Martin Chovanec's work at the Università della Svizzera italiana on Su
 import numpy as np
 
 
+
+
+# Создается класс. Где создаются экземпляры?
 class GeneticAlgorithm():
-    def __init__(self, fitness_function):
+    def __init__(self, fitness_function):  # Инициализация
         self.population = None
         self.fitness_function = fitness_function
         self.number_of_pairs = None
@@ -19,27 +22,27 @@ class GeneticAlgorithm():
         # Use single point crossover instead of uniform crossover
         self.single_point_cross_over = False
 
-    def generate_binary_population(self, size, genome_length):
+    def generate_binary_population(self, size, genome_length):  # Генерация бинарной популяции
         self.population = np.array([[not not x for x in line] for line in np.random.randint(0, 2, (size, genome_length))])
         self._update_fitness_vector()
         return self.population
 
-    def _generate_individual(self, genome_length):
+    def _generate_individual(self, genome_length):  # ?
         return np.random.randint(0, 2, (genome_length), dtype=bool
                                  )
-    def get_fitness_vector(self):
+    def get_fitness_vector(self):  # Функция пригодности
         return self.fitness_vector
 
-    def _update_fitness_vector(self):
+    def _update_fitness_vector(self):  # Обновление функции пригодности
         self.fitness_vector = [self.get_fitness(genom) for genom in self.population]
     def get_fitness(self, genome):
         return self.fitness_function(genome)
 
-    def get_best_genome(self):
+    def get_best_genome(self):  # Отбор
         self.best_genome = np.argmax(self.fitness_vector)
         return self.population[self.best_genome], self.fitness_vector[self.best_genome]
 
-    def run(self, iterations=1):
+    def run(self, iterations=1):  # Возможно, запуск ГА
         if self.population is None:
             raise RuntimeError("Population has not been generated yet.")
         if not self.number_of_pairs:
@@ -58,14 +61,14 @@ class GeneticAlgorithm():
                         self.population[worst_genome] = child
                         self.get_fitness_vector()[worst_genome] = child_fitness
 
-    def _get_parent_probabilities(self):
+    def _get_parent_probabilities(self):  # Возможности родителей. Возможно, селект
         # 2 − SP + 2* (SP −1)* (rank(i) −1) /(N −1)
         relative_fitness = self.fitness_vector / np.sum(self.fitness_vector)
         ranks_asc = np.argsort(relative_fitness)
         return np.array([2 - self.selective_pressure + 2 * (self.selective_pressure - 1)
                          * (ranks_asc[-i] - 1) / (len(ranks_asc) - 1) for i in range(len(ranks_asc))])
 
-    def _generate_children(self, parent_pair):
+    def _generate_children(self, parent_pair):  # Получение популяции детей. Возможно, скрещивание
         parent1 = parent_pair[0]
         parent2 = parent_pair[1]
         if self.single_point_cross_over:
@@ -90,7 +93,7 @@ class GeneticAlgorithm():
             return tuple(children)
 
 
-    def _select_parents(self, number_of_pairs, parent_probabilities):
+    def _select_parents(self, number_of_pairs, parent_probabilities):  # Получение родительской популяции
         """
         Create an array of pairs (array of parents)
         :param number_of_pairs:
@@ -134,7 +137,7 @@ class GeneticAlgorithm():
             # With this solution, it may happen that we have many pairs of the same pair :(
         return parent_pairs
 
-    def _mutate(self, genome, mutation_rate):
+    def _mutate(self, genome, mutation_rate):  # Оператор мутации. NumPy
         rnd = np.random.rand(len(genome))
         mutate_at = rnd < mutation_rate
         genome[mutate_at] = np.invert(genome[mutate_at])
